1ee8c66f3da9ca3a06c5a2f219172a7cb9c0df5f
Thanabodee Charoenpiriyakij <wingyminus@gmail.com>
2024-07-16 01:37:58 +0700

    Add doc
    Signed-off-by: Thanabodee Charoenpiriyakij <wingyminus@gmail.com>

--- elixir_wolfi/lib/elixir_wolfi/wolfi.ex
+++ elixir_wolfi/lib/elixir_wolfi/wolfi.ex
@@ -1,10 +1,16 @@
 defmodule ElixirWolfi.Wolfi do
+  @doc """
+  Wolfi base image.
+  """
   def base(dag) do
     dag
     |> Dagger.Client.container()
     |> Dagger.Container.from("cgr.dev/chainguard/wolfi-base")
   end
 
+  @doc """
+  Install Wolfi packages.
+  """
   def with_packages(container, packages) do
     container
     |> Dagger.Container.with_exec(["apk", "add"] ++ packages)
